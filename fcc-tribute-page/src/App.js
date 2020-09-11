14ff6c4318db937ee14ec2a2965c211ccec4a474
brandon quezada <brandon.cypaso@gmail.com>
2020-09-11 11:36:40 -0500

    couldn't get last user story to work, starting over with new approach

--- fcc-tribute-page/src/App.js
+++ fcc-tribute-page/src/App.js
@@ -5,42 +5,36 @@ import './App.css';
 function App() {
 	return (
 		<div className='App' id='main'>
-			<header className='App-header'>
-				<container>
-					<div>
-						<h1 id='title'>Dr. John Piper</h1>
+			<nav>
+				<ul>
+					<li>
+						<a href='#'>About</a>
+					</li>
+					<li>
+						<a href='#'>Popular Videos</a>
+					</li>
+					<li>
+						<a href='#'>Testimonials</a>
+					</li>
+				</ul>
+			</nav>
 
-						<p id='tribute-info'>
-							"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-							eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-							enim ad minim veniam, quis nostrud exercitation ullamco laboris
-							nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-							reprehenderit in voluptate velit esse cillum dolore eu fugiat
-							nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-							sunt in culpa qui officia deserunt mollit anim id est laborum."
-						</p>
-					</div>
-					<div>
-						<div id='img-div'>
-							<img
-								id='image'
-								src='https://upload.wikimedia.org/wikipedia/commons/1/14/John_Piper.jpg'
-								alt='John Piper head shot'
-							/>
-							<p id='img-caption'>Pastor Piper</p>
-						</div>
+			<div id='img-div'>
+				<img
+					id='image'
+					src='https://upload.wikimedia.org/wikipedia/commons/1/14/John_Piper.jpg'
+					alt='John Piper head shot'
+				></img>
 
-						<a
-							id='tribute-link'
-							href='https://www.desiringgod.org/'
-							target='_blank'
-							rel='noopener noreferrer'
-						>
-							Desiring God Website
-						</a>
-					</div>
-				</container>
-			</header>
+				<a
+					id='tribute-link'
+					href='https://www.desiringgod.org/'
+					target='_blank'
+					rel='noopener noreferrer'
+				>
+					Desiring God Website
+				</a>
+			</div>
 		</div>
 	);
 }
