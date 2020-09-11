3749bfdd86b9a6807fdeab9370ca7e50ba93a3ae
brandon quezada <brandon.cypaso@gmail.com>
2020-09-11 14:06:46 -0500

    changing up the structure of the site

--- fcc-tribute-page/src/App.js
+++ fcc-tribute-page/src/App.js
@@ -8,10 +8,10 @@ function App() {
 			<nav>
 				<ul>
 					<li>
-						<a href='#'>About</a>
+						<a href='#about'>About</a>
 					</li>
 					<li>
-						<a href='#'>Popular Videos</a>
+						<a href='#popular'>Popular Videos</a>
 					</li>
 					<li>
 						<a href='#'>Testimonials</a>
@@ -19,22 +19,107 @@ function App() {
 				</ul>
 			</nav>
 
-			<div id='img-div'>
-				<img
-					id='image'
-					src='https://upload.wikimedia.org/wikipedia/commons/1/14/John_Piper.jpg'
-					alt='John Piper head shot'
-				></img>
+			<section id='about'>
+				<div>
+					<h1>John Piper</h1>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+						eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+						ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+						aliquip ex ea commodo consequat. Duis aute irure dolor in
+						reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+						pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+						culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+					<p>Desiring God's YouTube Channel has over 4,200 video resources.</p>
 
-				<a
-					id='tribute-link'
-					href='https://www.desiringgod.org/'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Desiring God Website
-				</a>
-			</div>
+					<div
+						class='g-ytsubscribe'
+						data-channel='desiringGod'
+						data-layout='full'
+						data-count='default'
+					></div>
+				</div>
+				<div>
+					<img
+						id='image'
+						src='https://upload.wikimedia.org/wikipedia/commons/1/14/John_Piper.jpg'
+						alt='John Piper head shot'
+					></img>
+					<p>Dr. John Piper</p>
+				</div>
+			</section>
+
+			<section id='popular'>
+				<h2>Popular Videos</h2>
+				<div class='video-wrapper'>
+					<iframe
+						width='560'
+						height='315'
+						src='https://www.youtube.com/embed/xqgeT26BAnE'
+						frameborder='0'
+						allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+						allowfullscreen
+					></iframe>
+				</div>
+
+				<div class='video-wrapper'>
+					<iframe
+						width='560'
+						height='315'
+						src='https://www.youtube.com/embed/jLRue4nwJaA'
+						frameborder='0'
+						allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+						allowfullscreen
+					></iframe>
+				</div>
+
+				<div class='video-wrapper'>
+					<iframe
+						width='560'
+						height='315'
+						src='https://www.youtube.com/embed/n9QP-cqwyAw'
+						frameborder='0'
+						allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+						allowfullscreen
+					></iframe>
+				</div>
+
+				<div class='video-wrapper'>
+					<iframe
+						width='560'
+						height='315'
+						src='https://www.youtube.com/embed/YOKWdwSVRYo'
+						frameborder='0'
+						allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+						allowfullscreen
+					></iframe>
+				</div>
+
+				<div class='video-wrapper'>
+					<iframe
+						width='560'
+						height='315'
+						src='https://www.youtube.com/embed/nSSLLpVChng'
+						frameborder='0'
+						allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+						allowfullscreen
+					></iframe>
+				</div>
+			</section>
+
+			<section id='testimonials'>
+				<h2>Testimonials</h2>
+			</section>
+
+			<a
+				id='tribute-link'
+				href='https://www.desiringgod.org/'
+				target='_blank'
+				rel='noopener noreferrer'
+			>
+				Desiring God Website
+			</a>
 		</div>
 	);
 }
